ebb223d4c6c8dc9276452586f7c5a8d25bfff5b7
wwwrt <37418985+wwwrt@users.noreply.github.com>
2023-03-17 11:27:37 +0200

    Smiley face timo (#2)
    * circle
    
    * eyes
    
    * almostdone
    
    * fullface

--- scripts.js
+++ scripts.js
@@ -3,24 +3,35 @@ function setup() {
 }
 
 function draw () {
-    background("#8a2be2")
 
-    smileFaceCalin();
+    background("#a0daa9")
+    smileyFaceTimo();
 }
 
-function smileFaceCalin() {
-    //skin
-    fill("#fe00f6");
-    circle(200,360,35);
+function smileyFaceTimo () {
+    //head
+    fill ("#99999")
+    circle(200, 350, 30);
    
-    //right eye
-    circle(205,357,4);
-    circle(205,357,1);  //pupil
-    
-    //left eye
-    circle(195,357,4);
-    circle(195,357,1);  //pupil
-    //mouth
+   //eyes
+    fill ("#000000")
+    circle(205, 346, 4);
+    circle(196, 346, 4);
+  
+   //pupiles
 
-    arc(200,364,12,10,0, Math.PI);
-}
+    //mouth
+    fill ("#000000")
+    arc(200, 352, 20, 20, 0, Math.PI);
+    
+    fill ("#fffff")
+    rect(200, 352.5, 4, 4);
+    fill ("#fffff")
+    rect(196, 352.5, 4, 4);
+    
+    //sprancene
+    //line(192, 343, 330,290, );
+
+
+
+
