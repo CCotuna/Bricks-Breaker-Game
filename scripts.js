63edb89b92e76e902e76632703cef79c5467248d
Timo <tpripon99@gmail.com>
2023-03-17 10:53:41 +0200

    eyes

--- scripts.js
+++ scripts.js
@@ -8,5 +8,11 @@ function draw () {
 }
 
 function smileyFaceTimo () {
+    fill ("#99999")
     circle(200, 350, 30);
+    fill ("#000000")
+    circle(205, 347, 4);
+    circle(196, 347, 4);
+    
 }
+
