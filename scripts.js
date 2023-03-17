eb27ba56ed8f00f8b15c00e95d316ffe988a6c82
CCotuna <126149136+CCotuna@users.noreply.github.com>
2023-03-17 11:19:14 +0200

    Smile face calin (#1)
    * add circle - change background
    
    * add eyes
    
    * add mouth

--- scripts.js
+++ scripts.js
@@ -3,6 +3,24 @@ function setup() {
 }
 
 function draw () {
-    background("#ff0000")
+    background("#8a2be2")
+
+    smileFaceCalin();
 }
 
+function smileFaceCalin() {
+    //skin
+    fill("#fe00f6");
+    circle(200,360,35);
+   
+    //right eye
+    circle(205,357,4);
+    circle(205,357,1);  //pupil
+    
+    //left eye
+    circle(195,357,4);
+    circle(195,357,1);  //pupil
+    //mouth
+
+    arc(200,364,12,10,0, Math.PI);
+}
